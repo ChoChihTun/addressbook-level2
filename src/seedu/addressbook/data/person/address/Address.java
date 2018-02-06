da3f05317f6295bf3a74cc9af39d42a22192668e
Cho Chih Tun <33831669+ChoChihTun@users.noreply.github.com>
2018-02-06 20:46:11 +0800

    Address.java: add 4 new classes object

--- src/seedu/addressbook/data/person/address/Address.java
+++ src/seedu/addressbook/data/person/address/Address.java
@@ -1,4 +1,4 @@
-package seedu.addressbook.data.person;
+package seedu.addressbook.data.person.address;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
@@ -10,12 +10,16 @@ public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_VALIDATION_REGEX = "(\\d{1,3}.)?.+\\s(\\d{6})$";
+    public static final int BLOCK = 0;
+    public static final int STREET = 1;
+    public static final int UNIT = 2;
+    public static final int POSTAL_CODE = 3;
 
-    public final Block block;
-    public final Street street;
-    public final Unit unit;
-    public final PostalCode postalCode;
+    public final Block block = new Block();
+    public final Street street = new Street();
+    public final Unit unit = new Unit();
+    public final PostalCode postalCode = new PostalCode();
     private boolean isPrivate;
 
     /**
@@ -29,9 +33,20 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        System.out.println(trimmedAddress);
+        inputAddressDetails(trimmedAddress);
+    }
 
-        this.value = trimmedAddress;
+    /**
+     * Inputs address details into respective detail object
+     *
+     * @param trimmedAddress full address to be separated and input into respective detail object
+     */
+    private void inputAddressDetails(String trimmedAddress) {
+        String[] separatedAddressDetails = trimmedAddress.split("\\s*,\\s*");
+        block.inputBlock(separatedAddressDetails[BLOCK]);
+        street.inputStreet(separatedAddressDetails[STREET]);
+        unit.inputUnit(separatedAddressDetails[UNIT]);
+        postalCode.inputPostalCode(separatedAddressDetails[POSTAL_CODE]);
     }
 
     /**
@@ -43,7 +58,9 @@ public class Address {
 
     @Override
     public String toString() {
-        String stringAddress = block.getBlock() + ", " + street.getStreet() + ", " + unit.getUnit() + ", " + postalCode.getPostalCode();
+        String stringAddress = block.getBlock() + ", " + street.getStreet() + ", "
+                + unit.getUnit() + ", " + postalCode.getPostalCode();
+        stringAddress = stringAddress.replaceAll(", $", "");
         return stringAddress;
     }
 
@@ -51,7 +68,7 @@ public class Address {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.toString().equals(((Address) other).toString())); // state check
+                && this.toString().equals(other.toString())); // state check
     }
 
     @Override
