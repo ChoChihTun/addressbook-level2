a0a0d935155815ecd1f1ae1b30f1baecc4a71cb4
Cho Chih Tun <33831669+ChoChihTun@users.noreply.github.com>
2018-02-06 22:08:19 +0800

    Address.java: update EXAMPLE

--- src/seedu/addressbook/data/person/address/Address.java
+++ src/seedu/addressbook/data/person/address/Address.java
@@ -8,7 +8,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
+    public static final String EXAMPLE = "123, some street, #12-21, 123456";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = "(\\d{1,3}.)?.+\\s(\\d{6})$";
     public static final int BLOCK = 0;
