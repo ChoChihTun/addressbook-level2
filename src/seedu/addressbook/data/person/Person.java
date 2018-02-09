8cbc6449df414426c69ca57a4831bbe893f513c9
chochihtun <33831669+ChoChihTun@users.noreply.github.com>
2018-02-09 12:26:14 +0800

    Person.java: added sequenceNumber

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -9,11 +9,14 @@ import java.util.Objects;
  * Guarantees: details are present and not null, field values are validated.
  */
 public class Person implements ReadOnlyPerson {
+    private static final int INITIAL_SEQUENCE_NUMBER = 1;
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
+    private static int nextSequenceNumber = INITIAL_SEQUENCE_NUMBER;
 
     private final UniqueTagList tags;
     /**
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
